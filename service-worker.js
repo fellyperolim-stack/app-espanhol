609e8fec59a0c22a28a6d587a2d1f02c42151fb0
fellyperolim-stack <fellyperolim@gmail.com>
2026-07-17 18:53:11 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'espanolia-v10'; // ⚠️ AUMENTE ESTE NÚMERO sempre que atualizar o app,
+const CACHE = 'espanolia-v11'; // ⚠️ AUMENTE ESTE NÚMERO sempre que atualizar o app,
                                // assim o celular é forçado a baixar a versão nova.
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
